9405fc159bd3ab4075a8efbdeadbbc68810556b5
COR Coding <corcoding@Coding-16.lan>
2024-06-16 03:31:55 -0400

    added some details to the different endpoints

---
+++ npm/index.js
@@ -0,0 +1,56 @@
+
+const express = require("express")
+const app = express()
+bodyParser = require("body-parser")
+
+// using package managers to help set up swagger
+swaggerUi = require("swagger-ui-express")
+
+
+swaggerJsdoc = require("swagger-jsdoc")
+
+
+
+app.get('/', function (req, res) {
+  res.send('Swagger Documentation for Todo Purrsuit API')
+})
+
+
+// Using Swagger's Open Source Specification to set up documentation page (provided by Swagger)
+const options = {
+    definition: {
+      openapi: "3.1.0",
+      info: {
+        title: "LogRocket Express API with Swagger",
+        version: "0.1.0",
+        description:
+          "This is a simple CRUD API application made with Express and documented with Swagger",
+        license: {
+          name: "MIT",
+          url: "https://spdx.org/licenses/MIT.html",
+        },
+        contact: {
+          name: "LogRocket",
+          url: "https://logrocket.com",
+          email: "info@email.com",
+        },
+      },
+      servers: [
+        {
+          url: "https://todo-purrsuit-backend.vercel.app",
+        },
+      ],
+    },
+    apis: ["./routes/*.js"],
+  };
+  
+  const specs = swaggerJsdoc(options);
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+  );
+  
+app.listen(3000, 
+    console.log("Running on Port 3000")
+)
