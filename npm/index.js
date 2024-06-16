f42814431bb21cefc5fad24a931aa1824bd19469
COR Coding <corcoding@Coding-16.lan>
2024-06-16 03:46:45 -0400

    trying to deploy the swagger documentation

--- npm/index.js
+++ npm/index.js
@@ -15,41 +15,45 @@ app.get('/', function (req, res) {
   res.send('Swagger Documentation for Todo Purrsuit API')
 })
 
+app.get('/api-docs', function (req, res) {
 
-// Using Swagger's Open Source Specification to set up documentation page (provided by Swagger)
-const options = {
-    definition: {
-      openapi: "3.1.0",
-      info: {
-        title: "LogRocket Express API with Swagger",
-        version: "0.1.0",
-        description:
-          "This is a simple CRUD API application made with Express and documented with Swagger",
-        license: {
-          name: "MIT",
-          url: "https://spdx.org/licenses/MIT.html",
+  // Using Swagger's Open Source Specification to set up documentation page (provided by Swagger)
+  const options = {
+      definition: {
+        openapi: "3.1.0",
+        info: {
+          title: "LogRocket Express API with Swagger",
+          version: "0.1.0",
+          description:
+            "This is a simple CRUD API application made with Express and documented with Swagger",
+          license: {
+            name: "MIT",
+            url: "https://spdx.org/licenses/MIT.html",
+          },
+          contact: {
+            name: "LogRocket",
+            url: "https://logrocket.com",
+            email: "info@email.com",
+          },
         },
-        contact: {
-          name: "LogRocket",
-          url: "https://logrocket.com",
-          email: "info@email.com",
-        },
+        servers: [
+          {
+            url: "https://todo-purrsuit-backend.vercel.app",
+          },
+        ],
       },
-      servers: [
-        {
-          url: "https://todo-purrsuit-backend.vercel.app",
-        },
-      ],
-    },
-    apis: ["./routes/*.js"],
-  };
-  
-  const specs = swaggerJsdoc(options);
-  app.use(
-    "/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(specs)
-  );
+      apis: ["./routes/*.js"],
+    };
+    
+    const specs = swaggerJsdoc(options);
+    app.use(
+      "/api-docs",
+      swaggerUi.serve,
+      swaggerUi.setup(specs)
+    );
+})
+
+
   
 app.listen('https://todo-purrsuit-swagger.vercel.app', 
     // console.log("Running on Port 3000")
