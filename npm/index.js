52050abd0e7e50282ccc1c05179336540b9b6d19
COR Coding <corcoding@Coding-16.lan>
2024-06-16 03:52:39 -0400

    downgraded the versions

--- npm/index.js
+++ npm/index.js
@@ -15,43 +15,40 @@ app.get('/', function (req, res) {
   res.send('Swagger Documentation for Todo Purrsuit API')
 })
 
-app.get('/api-docs', function (req, res) {
-
-  // Using Swagger's Open Source Specification to set up documentation page (provided by Swagger)
-  const options = {
-      definition: {
-        openapi: "3.1.0",
-        info: {
-          title: "LogRocket Express API with Swagger",
-          version: "0.1.0",
-          description:
-            "This is a simple CRUD API application made with Express and documented with Swagger",
-          license: {
-            name: "MIT",
-            url: "https://spdx.org/licenses/MIT.html",
-          },
-          contact: {
-            name: "LogRocket",
-            url: "https://logrocket.com",
-            email: "info@email.com",
-          },
+// Using Swagger's Open Source Specification to set up documentation page (provided by Swagger)
+const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "LogRocket Express API with Swagger",
+        version: "1.0.0",
+        description:
+          "This is a simple CRUD API application made with Express and documented with Swagger",
+        license: {
+          name: "MIT",
+          url: "https://spdx.org/licenses/MIT.html",
         },
-        servers: [
-          {
-            url: "https://todo-purrsuit-backend.vercel.app",
-          },
-        ],
+        contact: {
+          name: "LogRocket",
+          url: "https://logrocket.com",
+          email: "info@email.com",
+        },
       },
-      apis: ["./routes/*.js"],
-    };
-    
-    const specs = swaggerJsdoc(options);
-    app.use(
-      "/api-docs",
-      swaggerUi.serve,
-      swaggerUi.setup(specs)
-    );
-})
+      servers: [
+        {
+          url: "https://todo-purrsuit-backend.vercel.app",
+        },
+      ],
+    },
+    apis: ["./routes/*.js"],
+  };
+  
+  const specs = swaggerJsdoc(options);
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+  );
 
 
   
