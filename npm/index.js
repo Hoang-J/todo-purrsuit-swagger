c108ee16e7359346331e2145b09746c0c749661f
COR Coding <corcoding@Coding-16.lan>
2024-06-16 12:29:56 -0400

    adjusted the url

--- npm/index.js
+++ npm/index.js
@@ -9,8 +9,7 @@ swaggerUi = require("swagger-ui-express")
 
 swaggerJsdoc = require("swagger-jsdoc")
 
-const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 
 app.get('/', function (req, res) {
@@ -49,7 +48,11 @@ const options = {
   app.use(
     "/api-docs",
     swaggerUi.serve,
-    swaggerUi.setup(specs, { customCssUrl: CSS_URL })
+    swaggerUi.setup(specs, {
+      customCss:
+        '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+      customCssUrl: CSS_URL,
+     })
   );
 
 
