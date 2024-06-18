4af040662367de3c987e89f5b85c5f889df55dce
COR Coding <corcoding@NwugochisiPhone.localdomain>
2024-06-18 11:06:47 -0400

    changed openapi version back to 3

--- npm/index.js
+++ npm/index.js
@@ -17,10 +17,10 @@ app.get('/', function (req, res) {
 // Using Swagger's Open Source Specification to set up documentation page (provided by Swagger)
 const options = {
     definition: {
-      openapi: "3.1.0",
+      openapi: "3.0.0",
       info: {
         title: "LogRocket Express API with Swagger",
-        version: "0.1.0",
+        version: "1.0.0",
         description:
           "This is a simple CRUD API application made with Express and documented with Swagger",
         license: {
